f95d3d9159d910805edfd221e1c98ea33f30239a
roi <roibuenaventura05@gmail.com>
2025-08-17 10:31:51 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+import app from "./app";
+import logger from "./utils/logger";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  logger.info(`🚀 Server running on port ${PORT}`);
+});
